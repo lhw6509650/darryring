be6b2b915899eb6009b69eb9e92283711ac78206
yianran <1784748050@qq.com>
2019-01-08 14:21:35 +0800

    我的收藏

--- src/main/java/com/darryring/service/Dr_collectService.java
+++ src/main/java/com/darryring/service/Dr_collectService.java
@@ -2,6 +2,9 @@ package com.darryring.service;
 
 import com.darryring.pojo.Dr_collect;
 
+import java.util.List;
+import java.util.Map;
+
 public interface Dr_collectService {
 
     //添加收藏
@@ -9,4 +12,11 @@ public interface Dr_collectService {
 
     //查询是否存在重复商品
     public Dr_collect selPro(Integer pid, Integer uid);
+
+    //查询当前用户的所有收藏
+    public List<Map<String,Object>> queAllCollect(Integer cid, Integer uid);
+
+    //移出收藏
+    public int removeCollect(Integer colId);
+
 }
